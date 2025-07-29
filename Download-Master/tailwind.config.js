2bb718b451e4d700b8a2e870d871b1bf9e08c9ca
Sahilk2807 <kamblesahil2807@gmail.com>
2025-07-29 14:18:37 +0530

    Create Download-Master/tailwind.config.js

---
+++ Download-Master/tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './static/js/**/*.js',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 10s linear infinite',
+      },
+      typography: (theme) => ({
+        dark: {
+          css: {
+            '--tw-prose-body': theme('colors.gray[300]'),
+            '--tw-prose-headings': theme('colors.white'),
+            '--tw-prose-lead': theme('colors.gray[400]'),
+            '--tw-prose-links': theme('colors.blue[400]'),
+            '--tw-prose-bold': theme('colors.white'),
+            '--tw-prose-counters': theme('colors.gray[400]'),
+            '--tw-prose-bullets': theme('colors.blue[400]'),
+            '--tw-prose-hr': theme('colors.gray[700]'),
+            '--tw-prose-quotes': theme('colors.gray[300]'),
+            '--tw-prose-quote-borders': theme('colors.gray[600]'),
+            '--tw-prose-captions': theme('colors.gray[400]'),
+            '--tw-prose-code': theme('colors.white'),
+            '--tw-prose-pre-code': theme('colors.gray[300]'),
+            '--tw-prose-pre-bg': theme('colors.gray[900]'),
+            '--tw-prose-th-borders': theme('colors.gray[600]'),
+            '--tw-prose-td-borders': theme('colors.gray[700]'),
+          },
+        },
+      }),
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
